5c84b46a33f33e0566d2d54993a41923c918fa78
Zakia6789 <junakiinfo23@gmail.com>
2024-10-29 20:48:04 +0600

    added add money form

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log('home added');
+// add money to the acccount
+
+// step 1: Added an event handler
+// prevent page reload after form submit 
+// step 2: get money to be added to the account
+
+// step 1: Add an event handler to the add money button insid the form 
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+          //prevent page reload after form submit
+    event.preventDefault();
+
+    // step 2: get money and pin to added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(addMoneyInput, pinNumberInput);
+
+})
