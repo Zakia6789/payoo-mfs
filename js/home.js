d075af9b0aa366228c76371c6379c58a1c885bca
Zakia6789 <junakiinfo23@gmail.com>
2024-10-29 22:28:34 +0600

    fourth commit

--- js/home.js
+++ js/home.js
@@ -2,7 +2,8 @@
 
 // step 1: Added an event handler
 // prevent page reload after form submit 
-// step 2: get money to be added to the account
+// step 2: get money to be added to the account balance and get the pin number
+// step 3: verify the pin number 
 
 // step 1: Add an event handler to the add money button insid the form 
 document.getElementById('btn-add-money').addEventListener('click', function(event){
@@ -12,6 +13,33 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // step 2: get money and pin to added to the account
     const addMoneyInput = document.getElementById('input-add-money').value;
     const pinNumberInput = document.getElementById('input-pin-number').value;
-    console.log(addMoneyInput, pinNumberInput);
+    console.log( addMoneyInput, pinNumberInput);
+
+    // step 3: verify the pin number (not the right way)
+    if(pinNumberInput ==='1234'){
+        console.log('adding money to your account');
+
+        // step 4: get the current balance
+    const balance = document.getElementById('account-balance').innerText;
+    console.log( balance);
+
+        // step 5: add addMoneyInput with balance 
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber ;
+        console.log(newBalance);
+
+
+        
+    // step 6: update the balance in the UI/DOM
+    document.getElementById('account-balance').innerText = newBalance;
+
+    }
+
+
+    else{
+        alert('Failed to add money! Please try again');
+    }
+
 
 })
